33fdf98537d78ba7d8572ebd9da7b58b4571c31e
OLIPLICHE <olipliche@gmail.com>
2022-07-26 02:25:41 +0100

    Add linters and update App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from Micro Lab P</h1>
+        <p>Here are Paka and Kossi</p>
+      </div>
+    );
+  }
+}
+
+export default App;
