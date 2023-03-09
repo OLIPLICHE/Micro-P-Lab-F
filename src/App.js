43cbed409f624656fddb6751807085145a624364
OLIPLICHE <olipliche@gmail.com>
2023-03-09 16:10:52 +0100

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import {
 import { useStateContext } from './contexts/ContextProvider';
 
 import './App.css';
-
+/* eslint-disable react/prop-types */
 const App = () => {
   const { activeMenu } = useStateContext();
 
