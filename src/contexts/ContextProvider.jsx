b4a1906a4b6bb767147a21e715b66802b4e31656
OLIPLICHE <olipliche@gmail.com>
2023-03-14 06:51:34 +0100

    Set active chat, notification, cart

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,21 +1,23 @@
 import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
-// const initialState = {
-//   chat: false,
-//   cart: false,
-//   userProfile: false,
-//   notification: false,
-// };
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
-
+  const [isClicked, setIsClicked] = useState(initialState);
   return (
     <StateContext.Provider
       value={{
         activeMenu,
         setActiveMenu,
+        isClicked,
+        setIsClicked,
       }}
     >
       {children}
