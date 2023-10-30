6c59d4fbac8e7cd58d5a8bdb14ab14a547c84afc
olipliche <olipliche@gmail.com>
2023-10-30 01:11:29 +0100

    Oppen the  Order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+// import { GridComponent, ColumnsDirective, CoumnDirective, Resize, Sort, ContextMenu,
+// Filter, Page, ExcelEport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+// Export { ordersData, contextMenuItems, ordersGrid } '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => (
-  <div>Orders</div>
+  <div><Header category="Page" title="Orders" /></div>
 );
 
 export default Orders;
