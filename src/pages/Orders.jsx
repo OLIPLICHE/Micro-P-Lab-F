102f54d4fac7ef0c8904a1e0ffa0e512fb000432
olipliche <olipliche@gmail.com>
2023-11-01 19:44:52 +0100

    Created Order table

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-// import { GridComponent, ColumnsDirective, CoumnDirective, Resize, Sort, ContextMenu,
+import { GridComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-grids';
+
+// import {  CoumnDirective, Resize, Sort, ContextMenu,
 // Filter, Page, ExcelEport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 // Export { ordersData, contextMenuItems, ordersGrid } '../data/core';
+import { ordersGridData } from '../data/core';
 import { Header } from '../components';
 
 const Orders = () => (
   <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
     <Header category="Page" title="Orders" />
+    {/* Now we CREATE the table item using syncfusion */}
+    <GridComponent>
+      <ColumnsDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {ordersGridData.map((item, index) => (<ColumnDirective key={index.id} {...item} />
+        ))}
+      </ColumnsDirective>
+    </GridComponent>
   </div>
 );
 
