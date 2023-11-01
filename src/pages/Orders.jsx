7fffb4952f7d0751a224532c9b0fbeb72896aa1a
olipliche <olipliche@gmail.com>
2023-11-01 14:51:25 +0100

    Created order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 // import { GridComponent, ColumnsDirective, CoumnDirective, Resize, Sort, ContextMenu,
 // Filter, Page, ExcelEport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
-// Export { ordersData, contextMenuItems, ordersGrid } '../data/dummy';
+// Export { ordersData, contextMenuItems, ordersGrid } '../data/core';
 import { Header } from '../components';
 
 const Orders = () => (
-  <div><Header category="Page" title="Orders" /></div>
+  <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="Page" title="Orders" />
+  </div>
 );
 
 export default Orders;
