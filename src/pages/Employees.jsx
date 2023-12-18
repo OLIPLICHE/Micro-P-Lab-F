589a1105ea2c522e22874fdbbd1f4bfc63b73079
OLIPLICHE <olipliche@gmail.com>
2023-12-18 10:43:01 +0100

    Complete customers page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   GridComponent, ColumnsDirective, ColumnDirective, Inject,
-  Page, Search, Edit,
+  Page, Search, Toolbar,
 } from '@syncfusion/ej2-react-grids';
 
 // Export { ordersData, contextMenuItems, ordersGrid, employeesData,employeesGrid }  '../data/core';
@@ -9,21 +9,23 @@ import { employeesData, employeesGrid } from '../data/core';
 import { Header } from '../components';
 
 const Employees = () => (
+
   <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="Page" title="Orders" />
+    <Header category="Page" title="Employees" />
     {/* Now we CREATE the table item using syncfusion */}
     <GridComponent
-      id="gridcomp"
       dataSource={employeesData}
       allowPaging
       allowSorting
+      toolbar={['Search']}
+      width="auto"
     >
       <ColumnsDirective>
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         {employeesGrid.map((item, index) => (<ColumnDirective key={index.id} {...item} />
         ))}
       </ColumnsDirective>
-      <Inject services={[Page, Edit, Search]} />
+      <Inject services={[Page, Search, Toolbar]} />
     </GridComponent>
   </div>
 );
